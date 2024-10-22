6fdd86d300e1b184b1dcae7c797e9051a4773b45
Martin Bond <28291143+martinbond7@users.noreply.github.com>
2024-10-22 11:37:28 +0100

    tasks.json added copy solution task postCreate.sh replaced git handling code to remove .git repos on fisrt build to remove git warnings on devconatiner startup

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -3,15 +3,14 @@ CONFIG=${1:-host}
 # move clang-format to home directory
 [[ -f .clang-format ]] && mv .clang-format ~
 
-# copy everyting except the src and include folders from the
+# copy everything except the src and include folders from the
 # taget project to avoid overwriting any src/include files on rebuild
 mkdir -p include src 
 cp -rf $(ls -d ~/projects/cmake-$CONFIG/* | grep -Ev '/?(src|include)/?$') .
 
-# reset git repro simulating a shallow fork of the project
-if [[ ! -d .git ]]; then 
-  git init -b main
+# remove .git repos that may cause warnings on first build with no src files
+if ! test -f src/main.*; then
+  echo "Remove .git folders on first build"
+  rm -rf .git 2>/dev/null
+  rm -rf *-[0-9][0-9][0-9]_exercises/.git 2>/dev/null
 fi
-git config --global --add safe.directory /home/feabhas/workspace
-git config core.safecrlf false
-git add -A
